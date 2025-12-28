9b610f3c8c669db051f33ef1aad920f45a434200
John Pinto <sevasguerrero@gmail.com>
2025-12-28 16:01:39 -0500

    AUTH UI

--- fronted-angular/src/app/auth/auth.component.ts
+++ fronted-angular/src/app/auth/auth.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import {FormsModule} from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    FontAwesomeModule
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+  email = '';
+  password = '';
+  isLoading = false;
+  errorMessage: string | null = null;
+  showResendVerification = false;
 
+  // FontAwesome icons and UI state
+  faEye = faEye;
+  faEyeSlash = faEyeSlash;
+  showPassword = false;
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
