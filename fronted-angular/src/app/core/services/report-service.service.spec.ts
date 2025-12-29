c76638a8a344b5be7425d3dd294d32b259bfce72
John Pinto <sevasguerrero@gmail.com>
2025-12-29 10:55:07 -0500

    added jasmine tests

--- fronted-angular/src/app/core/services/report-service.service.spec.ts
+++ fronted-angular/src/app/core/services/report-service.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ReportServiceService } from './report-service.service';
+import { IncidentReport } from '../models/report.model';
+import { environment } from '../../../environments/environment';
 
 describe('ReportServiceService', () => {
   let service: ReportServiceService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ReportServiceService]
+    });
     service = TestBed.inject(ReportServiceService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get incident report via GET', () => {
+    const dummyReport: IncidentReport = {
+      topAreas: [{ areaName: 'Area 1', incidentCount: 5 }],
+      criticalWeekly: [{ week: 'Week 1', incidentCount: 2 }]
+    };
+
+    service.getIncidentReport().subscribe(report => {
+      expect(report).toEqual(dummyReport);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiUrl}/incidents/reports`);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyReport);
   });
 });
--- fronted-angular/src/app/incidents/incidents.component.spec.ts
+++ fronted-angular/src/app/incidents/incidents.component.spec.ts
@@ -1,23 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { IncidentsComponent } from './incidents.component';
+import { IncidentsServiceService } from '../core/services/incidents-service.service';
+import { TokenStorageService } from '../core/services/token-storage.service';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('IncidentsComponent', () => {
   let component: IncidentsComponent;
   let fixture: ComponentFixture<IncidentsComponent>;
+  let incidentServiceSpy: jasmine.SpyObj<IncidentsServiceService>;
+  let tokenStorageSpy: jasmine.SpyObj<TokenStorageService>;
 
   beforeEach(async () => {
+    const iSpy = jasmine.createSpyObj('IncidentsServiceService', ['getAllIncidents']);
+    const tSpy = jasmine.createSpyObj('TokenStorageService', ['getUserRole']);
+
     await TestBed.configureTestingModule({
-      imports: [IncidentsComponent]
+      imports: [IncidentsComponent, HttpClientTestingModule],
+      providers: [
+        { provide: IncidentsServiceService, useValue: iSpy },
+        { provide: TokenStorageService, useValue: tSpy }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(IncidentsComponent);
     component = fixture.componentInstance;
+    incidentServiceSpy = TestBed.inject(IncidentsServiceService) as jasmine.SpyObj<IncidentsServiceService>;
+    tokenStorageSpy = TestBed.inject(TokenStorageService) as jasmine.SpyObj<TokenStorageService>;
+
+    incidentServiceSpy.getAllIncidents.and.returnValue(of([]));
+    tokenStorageSpy.getUserRole.and.returnValue('USER');
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load incidents on init', () => {
+    const dummyIncidents = [{ id: 1, title: 'Test' }];
+    incidentServiceSpy.getAllIncidents.and.returnValue(of(dummyIncidents));
+
+    component.ngOnInit();
+
+    expect(incidentServiceSpy.getAllIncidents).toHaveBeenCalled();
+    expect(component.incidents).toEqual(dummyIncidents as any);
+  });
+
+  it('should check if user is supervisor', () => {
+    tokenStorageSpy.getUserRole.and.returnValue('SUPERVISOR');
+    expect(component.isSupervisor()).toBeTrue();
+
+    tokenStorageSpy.getUserRole.and.returnValue('USER');
+    expect(component.isSupervisor()).toBeFalse();
   });
 });
